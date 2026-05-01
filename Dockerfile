20d4de41714de7dc429793823ee189b07d58bb47
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-05-01 23:34:26 +0100

    remote-admin

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ RUN apk add --no-cache \
     bind \
     bind-tools \
     bash
-RUN mkdir -p /etc/bind /var/cache/bind /var/lib/bind \
-    && chown -R named:named /etc/bind /var/cache/bind /var/lib/bind
+RUN mkdir -p /etc/bind /var/cache/bind /var/lib/bind /var/log \
+    && chown -R named:named /etc/bind /var/cache/bind /var/lib/bind /var/log
 EXPOSE 53/udp 53/tcp
 CMD ["named", "-g", "-u", "named", "-c", "/etc/bind/named.conf"]
---
+++ web_ui/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+import dns.resolver
+import socket
+
+app = Flask(__name__)
+
+DNS_SERVER = "bind9_dns"
+
+def dns_lookup(domain):
+  
+    try:
+        resolver = dns.resolver.Resolver()
+        
+        # Resolve the Docker service name to its internal IP
+        # Use the service name 'dns-server' from your yaml
+        server_ip = socket.gethostbyname("dns-server") 
+        
+        resolver.nameservers = [server_ip]
+        answer = resolver.resolve(domain, "A")
+        return answer[0].to_text()
+    except Exception as e:
+        return f"Lookup failed: {e}"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    domain = ""
+
+    if request.method == "POST":
+        domain = request.form["domain"]
+        result = dns_lookup(domain)
+
+    return render_template("index.html", result=result, domain=domain)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
